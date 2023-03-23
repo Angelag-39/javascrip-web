90c9a6a422e46fe07428c9603016be07980fa04a
angela <angelaguacaran@gmail.com>
2023-03-22 22:39:26 -0300

    .

---
+++ javascrit+html/js/programa2.js
@@ -0,0 +1,12 @@
+function desenhaQuadradoVerde(){
+
+var tela = document.querySelector('canvas');
+var pincel = tela.getContext('2d');
+
+pincel.fillStyle ='green';
+pincel.fillRect(0,0,50,50);
+pincel.fillStroke ='black';
+pincel.strokeRect(0,0,50,50);
+
+}
+desenhaQuadradoVerde();
