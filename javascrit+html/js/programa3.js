c0e153f0bbf9447dec8484a47ebb80908728ed60
angela <angelaguacaran@gmail.com>
2023-03-28 23:12:16 -0300

    .

---
+++ javascrit+html/js/programa3.js
@@ -0,0 +1,55 @@
+var tela = document.querySelector('canvas');
+var pincel = tela.getContext('2d');
+
+pincel.fillStyle ='lightgray';
+pincel.fillRect(0,0,600,400);
+
+function desenhaCirculo(x,y, raio){
+    pincel.fillStyle = 'blue';
+    pincel.beginPath();
+    pincel.arc(x, y, raio,0, 2*Math.PI);
+    pincel.fill();
+}
+
+function limpaTela(){
+    pincel.clearRect(0,0,600,400);
+}
+
+var x=20;
+
+function atualizaTela(params) {
+    limpaTela();
+    desenhaCirculo(x,20,10);
+    x++;
+}
+setInterval(atualizaTela,10);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function desenhaCirculo(evento) {
+
+    var x = evento.pageX - tela.offsetLeft;
+    var y = evento.pageY - tela.offsetTop;
+    pincel.fillStyle = 'blue';
+    pincel.beginPath();
+    pincel.arc(x , y, 10,0,2 * 3.14);
+    pincel.fill();
+    console.log(x + ','+ y);
+    
+    
+}
+tela.onclick = desenhaCirculo;*/
+
